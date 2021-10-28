e626350d320a10dbe529479b9e646218e74e723a
start.vaadin.com <no-reply@vaadin.com>
2021-10-28 13:52:53 +0000

    Generated project

---
+++ src/main/java/de/davidemarcoli/views/weather/WeatherView.java
@@ -0,0 +1,30 @@
+package de.davidemarcoli.views.weather;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("Weather")
+@Route(value = "")
+public class WeatherView extends VerticalLayout {
+
+    public WeatherView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
